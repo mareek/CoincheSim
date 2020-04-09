0e8c95860593c4d824fb10c2aeafe825c1c3199f
Matthieu Mourisson <mareek@gmail.com>
2020-04-09 23:35:07 +0200

    created AnnonceLoop function

--- Global.lua
+++ Global.lua
@@ -39,32 +39,55 @@ function GameLoop()
     TeamEW.gameScore = 0
 
     local firstPlayerIndex = 1
-    while true do
+    while TeamNS.gameScore < 1000 and TeamEW.gameScore < 1000 do
         local roundInfo = AnnonceLoop(firstPlayerIndex)
-        RoundLoop(roundInfo)
-
-        if TeamNS.gameScore >= 1000 or TeamEW.gameScore >= 1000 then
-            break
-        else
-            firstPlayerIndex = GetNextPlayerIndex(firstPlayerIndex)
+        if roundInfo ~= nil then
+            RoundLoop(roundInfo, firstPlayerIndex)
         end
+        firstPlayerIndex = GetNextPlayerIndex(firstPlayerIndex)
     end
+
     local winner = (TeamNS.gameScore >= 1000) and TeamNS or TeamEW;
 end
 
-function AnnonceLoop(firstPlayerIndex) end
+function AnnonceLoop(firstPlayerIndex)
+    local currentStake = nil
+    local passCount = 0
+    local runningTeam = nil
+    local isCoinche = false;
+    local currentPlayerIndex = firstPlayerIndex
 
-function CreateRoundInfo(team, atoutColor, contract, isCoinche)
-    return {
-        team = team,
-        otherTeam = (team == TeamNS) and TeamNS or TeamEW,
-        atoutColor = atoutColor,
-        contract = contract,
-        isCoinche = isCoinche
-    }
+    while passCount < 3 or (passCount == 3 and runningTeam == nil) do
+        local currentPlayer = PlayersInOrder[currentPlayerIndex]
+        local playerStake = GetPlayerStake(currentPlayer)
+        if playerStake == "coinche" then
+            isCoinche = true
+            break
+        elseif playerStake == nil then
+            passCount = passCount + 1
+        else
+            passCount = 0
+            currentStake = playerStake
+            runningTeam = currentPlayer.team
+        end
+
+        currentPlayerIndex = GetNextPlayerIndex(currentPlayerIndex)
+    end
+
+    if currentStake == nil then
+        return nil
+    else
+        return {
+            team = runningTeam,
+            otherTeam = (runningTeam == TeamNS) and TeamNS or TeamEW,
+            atoutColor = currentStake.atoutColor,
+            contract = currentStake.contract,
+            isCoinche = isCoinche
+        }
+    end
 end
 
-function RoundLoop(roundInfo)
+function RoundLoop(roundInfo, firstPlayerIndex)
     local runningTeam = roundInfo.team
     local otherTeam = roundInfo.otherTeam
     otherTeam.roundScore = 0
